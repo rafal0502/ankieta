889bf2f2e9ec7f08ce8c0efb82e71702e48a1be3
rafal0502 <rafalchojnacki0502@gmail.com>
2019-04-07 10:56:48 -0700

    Added new templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
-from django.template import loader
-
 from .models import Question
 
 # Create your views here.
@@ -9,10 +7,7 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
+    context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
 
